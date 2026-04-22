b83778dd18487a9177538ab89fdcbaab68715292
ariel raymundo <ariraymundo98@gmail.com>
2026-04-22 11:15:45 -0700

    added code

--- src/main/java/com/pluralsight/BuildYourOwnAdventure.java
+++ src/main/java/com/pluralsight/BuildYourOwnAdventure.java
@@ -1,10 +1,42 @@
 package com.pluralsight;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 
-public class Main {
-    static void main() {
+public class BuildYourOwnAdventure {
+    static void main()
+    {
+     loadAdventure();
+    }
 
-        }
+
+
+
+    // function that reads the .csv
+    public static void loadAdventure()
+    {
+       try
+       {
+           FileReader fileReader = new FileReader("adventure1.csv");
+           BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+           // skips the header
+           String lines = bufferedReader.readLine();
+
+           // reads the file
+           while((lines = bufferedReader.readLine()) != null) {
+               System.out.println(lines);
+           }
+
+           // closes the file
+           bufferedReader.close();
+       }
+
+       catch(Exception ex)
+       {
+           System.out.println(ex.getMessage());
+       }
+    }
 }
 
