258cb72e0d8029d761d43b59ad4743acdb4dfa60
ariel raymundo <ariraymundo98@gmail.com>
2026-04-22 13:55:43 -0700

    added array

--- src/main/java/com/pluralsight/BuildYourOwnAdventure.java
+++ src/main/java/com/pluralsight/BuildYourOwnAdventure.java
@@ -28,8 +28,23 @@ public class BuildYourOwnAdventure {
            // reads the .csv file
            while((lines = bufferedReader.readLine()) != null)
            {
-               System.out.println();
-               System.out.println(lines);
+
+               // array to split the .csv into columns
+
+               String[] split = lines.split("\\|");
+               int id = Integer.parseInt(split[0]);
+               String storyText = split[1];
+               String option1Text = split[2];
+               int option1NextId = Integer.parseInt(split[3]);
+               String option2Text = split[4];
+               int option2NextId = Integer.parseInt(split[5]);
+
+               // creates storystep object
+               StoryStep storystep = new StoryStep(id, storyText, option1Text,
+                       option1NextId, option2Text, option2NextId);
+
+               System.out.println(storystep.getStoryText());
+
            }
 
            // closes the file
