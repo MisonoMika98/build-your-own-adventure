6537f39f099905e68b7fbee426e8a085800a5ae4
ariel raymundo <ariraymundo98@gmail.com>
2026-04-22 15:46:12 -0700

    added functions WIP

--- src/main/java/com/pluralsight/BuildYourOwnAdventure.java
+++ src/main/java/com/pluralsight/BuildYourOwnAdventure.java
@@ -2,21 +2,72 @@ package com.pluralsight;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class BuildYourOwnAdventure {
+    // static and at top of class so it can run it static functions and out of scope
+    static ArrayList<StoryStep> steps;
 
     static void main()
     {
-     loadAdventure();
+     steps = loadAdventure();
+     homeScreen();
+    }
+
+    public static void homeScreen()
+    {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Welcome to your adventure!");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Enter (P) to Play");
+        System.out.println("Enter (Q) to Quit");
+        System.out.println("===================");
+        String selection = userInput.nextLine().toUpperCase().trim();
+        // user input options
+        if (selection.equals("P")) {
+            gameScreen(1);
+        }
+        else if (selection.equals("Q")) {
+            System.out.println();
+            System.out.println("See you again!");
+            userInput.close();
+        }
+        else {
+            System.out.println();
+            System.out.println("Error, please try again!");
+            homeScreen(); // recursion
+        }
+
+    }
+
+    public static void gameScreen(int id)
+    {
+        // gets arraylist size
+        for (int i = 0; i < steps.size(); i++) {
+            StoryStep step = steps.get(i);
+            // compares id getter to id in the array list
+            if (step.getId() == id) {
+                System.out.println();
+                System.out.println("Story Text: " + step.getStoryText());
+                System.out.println();
+                System.out.println("1) " + step.getOption1Text());
+                System.out.println();
+                System.out.println("2) " + step.getOption2Text());
+            }
+        }
     }
 
 
-
-
-    // function that reads the .csv
-    public static void loadAdventure()
+    // function that reads the .csv and stores data
+    public static ArrayList <StoryStep> loadAdventure()
     {
+        // create the container
+        // arraylists grow as needed when you add new items, unlike an array with a number of predetermined items
+       ArrayList<StoryStep> steps = new ArrayList<>();
+
        try {
            FileReader fileReader = new FileReader("adventure1.csv");
            BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -27,13 +78,14 @@ public class BuildYourOwnAdventure {
            // reads the .csv file
            while ((lines = bufferedReader.readLine()) != null) {
 
-               // array to columns the .csv into columns
+               // array to split the .csv into columns
                String[] columns = lines.split("\\|");
                int id = Integer.parseInt(columns[0]);
                String storyText = columns[1];
                String option1Text = columns[2];
                int option1NextId = Integer.parseInt(columns[3]);
                String option2Text = columns[4];
+               // changed text (END) in the .cvs file column[5] to -1 so it can read the column
                int option2NextId = Integer.parseInt(columns[5]);
 
 
@@ -41,7 +93,9 @@ public class BuildYourOwnAdventure {
                StoryStep storyStep = new StoryStep(id, storyText, option1Text,
                        option1NextId, option2Text, option2NextId);
 
-               System.out.println(storyStep.getStoryText());
+               // adds the current storyStep into an array list/container
+               steps.add(storyStep);
+
            }
 
 
@@ -53,6 +107,8 @@ public class BuildYourOwnAdventure {
        {
            System.out.println(ex.getMessage());
        }
+       // returns the container
+       return steps;
     }
 
 
